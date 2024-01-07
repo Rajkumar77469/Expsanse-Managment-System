82a103eca3c6537df30bc82ca3f88c83153379fa
rajkumar <rajkumarrajput806u@example.com>
2024-01-07 22:47:03 +0530

    first commit

---
+++ client/src/pages/Login.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from "react";
+import { Form, Input, message } from "antd";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import Spinner from "../components/Spinner";
+import "../styles/Loginpage.css";
+
+const Login = () => {
+  const img =
+    "https://juntrax.com/blog/wp-content/uploads/2020/10/Expenses-Management-System-Featured-Juntrax.jpg";
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  //from submit
+  const submitHandler = async (values) => {
+    try {
+      setLoading(true);
+      const { data } = await axios.post("/users/login", values);
+      setLoading(false);
+      message.success("login success");
+      localStorage.setItem(
+        "user",
+        JSON.stringify({ ...data.user, password: "" })
+      );
+      navigate("/");
+    } catch (error) {
+      setLoading(false);
+      message.error("something went wrong");
+    }
+  };
+
+  //prevent for login user
+  useEffect(() => {
+    if (localStorage.getItem("user")) {
+      navigate("/");
+    }
+  }, [navigate]);
+  return (
+    <>
+      <div className="login-page ">
+        {loading && <Spinner />}
+        <div className="row container">
+          <h1 className="text-center">Expsanse Managment System</h1>
+          <div className="col-md-6">
+            <img src={img} alt="login-img" width={"100%"} height="100%" />
+          </div>
+          <div className="col-md-4 login-form">
+            <Form layout="vertical" onFinish={submitHandler}>
+              <h1>Login Form</h1>
+
+              <Form.Item label="Email" name="email">
+                <Input type="email" required />
+              </Form.Item>
+              <Form.Item label="Password" name="password">
+                <Input type="password" required />
+              </Form.Item>
+              <div className="d-flex justify-content-between">
+                <Link to="/register">
+                  Not a user ? Click Here to regsiter !
+                </Link>
+                <button className="btn ">Login</button>
+              </div>
+            </Form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
